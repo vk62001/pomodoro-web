4783bcb7a59e71816592e7e67ea1826c37bfae58
MonicaValenciaCode <monicacoding@outlook.com>
2023-06-08 23:48:12 +0100

    Add sounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Pomodoro } from './components/Pomodoro'
 
--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -2,33 +2,40 @@ import { useState, useEffect } from 'react'
 import { Watch } from "../Watch/Watch";
 import './Timer.css'
 import { Options } from "../Options/Options";
+import Click from '../../assets/sounds/click.wav'
+import TimeOver from '../../assets/sounds/time_over.wav'
 
 interface IWatch {
     bgColour: any
 }
 
 export const Timer = ({ bgColour }: IWatch) => {
-    const [seconds, setSeconds] = useState<number>(0);
-    const [start, setStart] = useState(false);
-    const [mode, setMode] = useState(0);
-    const [resetClicked, setResetClicked] = useState(false);
-    const [startClicked, setStartClicked] = useState(false);
+    const [seconds, setSeconds] = useState<number>(1500);
+    const [start, setStart] = useState<boolean>(false);
+    /**mode: controls behaviour on options picked. (cannot use seconds as it change, but mode keeps the value until another choice is picked)*/
+    const [mode, setMode] = useState<number>(0);
+
+    /** These both controls style changes on button click rather than functionality*/
+    const [resetClicked, setResetClicked] = useState<boolean>(false);
+    const [startClicked, setStartClicked] = useState<boolean>(false);
 
 
     useEffect(() => {
         //if no params, only launch once. If you pass params, when the variables change, it renders
         let timer: any = null;
 
+        //clear timer when we click in an option and set start to false
         if (seconds === 0 && start) {
             clearInterval(timer)
             setStart(!start);
+            playAlertSound(TimeOver);
             return;
         }
 
         if (start) {
             timer = setInterval(() => {
                 setSeconds(seconds - 1)
-            }, 1000);
+            }, 10);
 
 
         } else if (!start || seconds !== 0) {
@@ -41,7 +48,7 @@ export const Timer = ({ bgColour }: IWatch) => {
 
     }, [start, seconds]); //que es este arreglo al final? se puede usar un objeto vacio tambien? o otro tipo de data collection?
 
-    const timerSetter = (seconds: number) => {
+    const timerSetter = (seconds: number): void => {
         setSeconds(seconds)
         setMode(seconds)
 
@@ -52,7 +59,6 @@ export const Timer = ({ bgColour }: IWatch) => {
 
         } else if (seconds === 900) {
             bgColour("bg-long")
-
         }
 
         if (seconds === 1500 || seconds === 900 || seconds === 300) {
@@ -61,23 +67,29 @@ export const Timer = ({ bgColour }: IWatch) => {
         }
     }
 
-    const reset = () => {
-        setSeconds(0);
+    const reset = (): void => {
+        setSeconds(mode);
         setResetClicked(true);
-        setMode(0);
+        setStart(false);
         setStartClicked(false);
         setTimeout(() => {
             setResetClicked(false);
         }, 200);
     }
 
-    const startSetter = () => {
+    const startSetter = (): void => {
+        playAlertSound(Click);
         setStart(!start);
         setStartClicked(true);
         setTimeout(() => {
             setStartClicked(false);
         }, 200);
     }
+
+    const playAlertSound = (sound: any): void => {
+        const audio = new Audio(sound);
+        audio.play();
+    };
 
     return (
         <div className="timer">
@@ -94,7 +106,7 @@ export const Timer = ({ bgColour }: IWatch) => {
             </div>
             <Watch seconds={seconds} />
             <div className="buttons">
-                <button className={startClicked ? "button-clicked" : "button start"} onClick={startSetter}>{!start ? 'Start' : 'Pause'}</button>
+                <button disabled={seconds === 0} className={startClicked ? "button-clicked" : "button start"} onClick={startSetter}>{!start ? 'Start' : 'Pause'}</button>
                 <button
                     className={resetClicked ? "button-clicked" : "button reset"}
                     onClick={reset}>
