407108e17570312db17f18f614e67969bef0b571
eder <eder@Mac-mini-de-eder.local>
2023-06-09 15:47:27 -0600

    add custom hook

---
+++ src/CustomHook/useTimer.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+const useTimer = (initialTime: number = 0) => {
+  const [time, setTime] = useState<number>(initialTime);
+  const [isRunning, setIsRunning] = useState<boolean>(true);
+  
+  let timerId: ReturnType<typeof setInterval>;
+
+  const stopTimer = () => {
+    setTime(initialTime);
+    setIsRunning(false);
+  };
+
+  const startTimer = () => {
+    timerId = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    
+    if (isRunning) {
+      startTimer();
+    } else {
+      clearInterval(timerId);
+    }
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [initialTime, isRunning]);
+
+  return { time, isRunning, setIsRunning, stopTimer };
+};
+
+export default useTimer;
+
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,24 @@
+import useTimer from '../CustomHook/useTimer';
+
 export const Todo = () => {
+    const { time, isRunning, setIsRunning, stopTimer } = useTimer();
+
+  const handleToggleTimer = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning);
+  };
+
+  const handleResetTimer = () => {
+    if (stopTimer) {
+      stopTimer();
+    }
+    setIsRunning(true);
+  };
     return (
+        <>
         <div>Todo</div>
+        <h1>Timer: {time} segundos</h1>
+         <button onClick={handleToggleTimer}>{isRunning ? 'Detener' : 'Iniciar'}</button>
+            <button onClick={handleResetTimer}>Reiniciar</button>
+        </>
     )
 }
